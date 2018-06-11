3fa0691610d9b0bd89e0dc3b41f245b25088a642
temilolakutelu <temilolakutelu@gmail.com>
2018-06-11 11:53:41 +0100

    api call

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-$('.card').hide("fast");
+// $('.card').hide("fast");
 $(document).ready(function () {
   var fahrenheit, celsius;
   var weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather";
   var apiKey = "0e63f730e5627c7012970b7b8f4d63fe"; //<weather-api-keys
-  
+
+
 
   $("#btn").click(function () {
 
     getWeatherData(weatherApiUrl, apiKey);
     $('.card').show("slow");
+
     document.getElementById("myForm").reset();
   });
 
@@ -26,11 +28,15 @@ $(document).ready(function () {
         var temprature = data.main.temp;
         celsius = temprature;
         fahrenheit = celsius * 1.8 + 32;
+        var country = data.sys.country;
+        var pressure = data.main.pressure;
+        var humidity = data.main.humidity;
         var icon = data.weather[0].icon;
         var weatherDetail = data.weather[0].main + ", " + data.weather[0].description;
+        var wind = data.wind.speed + "," + data.wind.deg;
 
-        console.log(temprature, place);
-        weatherDisplay(place, weatherDetail, temprature, icon);
+        console.log(temprature, place, country, pressure, humidity, weatherDetail);
+        weatherDisplay(place, country, weatherDetail, temprature, icon, wind, pressure, humidity);
       },
       error: function (err) {
         alert('Oops something went wrong, Please try again.');
@@ -43,12 +49,16 @@ $(document).ready(function () {
   });
 
 
-  function weatherDisplay(place, weatherDetail, temprature, icon) {
+  function weatherDisplay(place, country, weatherDetail, temprature, icon, wind, pressure, humidity) {
 
-    $('#cityName').html(place);
+    $('#cityName').html(place + " " + country);
+
     $('.iconpic').attr('src', 'http://openweathermap.org/img/w/' + icon + '.png'); //update the icon based on weather
 
-    $('.temp').html(temprature + "&#8451;"); //update the temprature
+    $('.temp').append(temprature + "&#8451;"); //update the temprature
+    $('.humidity').append(humidity);
+    $('.pressure').append(pressure);
+    $('.wind').append(wind);
     $('.weatherDetail').html(weatherDetail); //update weather description in html
   }
 
